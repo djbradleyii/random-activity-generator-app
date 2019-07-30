fff1b2058feca926c39211e34909d25717fbdc5a
David B <d.jbradley.ii@gmail.com>
2019-07-30 13:00:38 -0700

    adds function stubs

---
+++ index.js
@@ -0,0 +1,27 @@
+function displayYouTubeResults(){
+    // this function will display youtube results
+    console.log('displayYouTubeResults ran');
+
+}
+
+function getYouTubeResults(){
+    // this function will get youtube results
+    console.log('getYouTubeResults ran');
+}
+
+function displayActivity(){
+    //this function will display the activity to the DOM
+    console.log(`displayActivity ran`);
+}
+
+function getActivity(activityType){
+    //this function will get the activity
+    console.log(`getActivity ran`);
+}
+
+function handleFormSubmission(){
+// this function will handle the form submission
+console.log(`handleFormSubmission ran`);
+}
+
+$(handleFormSubmission);
