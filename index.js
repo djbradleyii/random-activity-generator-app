b686eb974895e6450171ddc0cb9ec1a7fd54fd50
David B <d.jbradley.ii@gmail.com>
2019-08-03 23:13:43 -0700

    updates help and welcome modals

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function displayHelp(){
                         <dt>Activity</dt>
                         <dd>Randomly generated activity.</dd>
                         <dt>Accessibility</dt>
-                        <dd>Accessible Activities are based on a factor describing how possible an event is to do, with zero being the most accessible. This is set to a range of 0.0 - 2.0.
+                        <dd>Accessible Activities are based on a factor describing how possible an event is to do, with zero being the most accessible. This is set to a range of 0.0 - 1.0.
                         </dd>
                         <dt>Category</dt>
                         <dd>You can choose a specific category or go truly random. Category list: Education, Recreational, Social, Diy, Charity, Cooking, Relaxation, Music or Busywork.
@@ -146,12 +146,20 @@ function displayHelp(){
                         <dd>Recommended Number of participants for the activity.
                         </dd>
                         <dt>Price</dt>
-                        <dd>Price is based on a factor describing the potential cost of the event, with zero being free. The range is from 0.0 - 1.0. This is not an exact science, but aims to be an indicator of the price obligiation for each activity.
+                        <dd>Price indicator is based on a factor describing the potential cost of the event, with zero being free. The range is from 0.0 - 1.0. This is not an exact science, but aims to be an indicator of the price obligation for each activity.
                         </dd>
                         <dt>YouTube Videos</dt>
-                        <dd>As each activity is generated, YouTube is searched for related videos based on the activity generated. The results of the YouTube search will be listed below the activity information. Clicking on the thumbnail will send you to YouTube to view the video.
+                        <dd>As each activity is generated, YouTube is searched for related videos based on the activity generated. The results of the YouTube search will be listed below the activity information. Clicking on the thumbnail will send you to YouTube to view the video or the YouTube channel.
                         </dd>
                     </dl>
+                    <p>Some things to note as I help you with generating activities.</p>
+                    <ol class="notes">
+                        <li>As I was intended to provide a random activity, I made it so that you cannot alter the number of participants or change the price requirements. Maybe I will change that in the future.</li>
+                        <li>If the activity suggests more participants than you have, feel free to get creative. For example: "Go to an escape room" suggests that four participants be involved. If you are alone or have fewer than four participants, you can take this opportunity to catch up with friends or team-build with your coworkers.</li>
+                        <li> Don't count it out just because you might not have the exact number of participants.</li>
+                        <li>Lastly, the price indicator is there to give an indication of whether or not you might need money for the activity. This is a best guess and not an exact science.</li>
+                    </ol>
+                    <p>Do remember that each time you press the button, all of the previous information goes away never to return again. Just kidding, but it will go away until it is randomly generated again. Have fun!!</p>
                     <button type="button" class="close">Close</button>
                 </div>
             </div>     
@@ -178,20 +186,12 @@ function displayIntro(){
                     <span class="introCloseBtn">&times;</span>
                 </div>
                 <div class="introModal-content">
-                    <p>Hi there, I was created to help you with your boredom. Wait, what's that? You're not bored? Well I was also created to generate a random activity for you to do alone or with friends.</p>
+                    <p>Hi there! Are you bored out of your mind? I was created to help you with just that. Wait, what's that? You're not bored? Well I am here to give you a random activity to do alone or with friends.</p>
                     <ol>
-                        <li class="step">Step 1: Choose a category or go full random. You have eight categories from which to choose. I suggest random, but what do I know? I do as I am told.</li> 
-                        <li class="step">Step 2: Decide if you would like for the activity to be "Accessible" or again, go fully random. If checked, I attempt to return activities that can be done by almost anyone. Now, I am not perfect (blame the person who programmed me), but I do my best.</li>  
-                        <li class="step">Step 3: Click the generate random activity button. This is the fun part. A random activity is provided!! Well look at that. I am awesome aren't I? I also sent the activity to YouTube, since I am "connected", and well look at that! They sent back some videos related to the activity provided. How awesome are they? If you find that the videos aren't helpful, let me know and I will launch a formal complaint.</li>  
-                        <li class="step">Step 4: Now this is the most important step. GO OUT AND ENJOY THE ACTIVITY!! I am just a bunch of 1s and 0s, so can't join you, but do enjoy.</li>    
+                        <li class="step">Step 1: Choose a category or go full random. You have 8 categories from which to choose (choose wisely or don't). You can also select 'Accessible' activities, which is my attempt at returning activities that can be done by almost anyone, but I am not perfect (blame the person who programmed me), but I do my best.</li> 
+                        <li class="step">Step 2: Now the fun part. Click the 'Generate Activity' button and BOOM! A random activity at your fingertips. I'm awesome, aren't I? I also sent the activity to YouTube, since I am "connected", and well look at that! They sent back some videos related to the activity I provided. How awesome are they? If you find that the videos aren't helpful, let me know and I will launch a formal complaint with them.</li>  
+                        <li class="step">Step 3: Now, the most important step: GO OUT AND ENJOY THE ACTIVITY! I am just a bunch of 1s and 0s, so I can't join you, but do have all the fun!</li>  
                     </ol>
-                    <p>Some things to note as I help you with generating activities.</p>
-                    <ol class="notes">
-                        <li>As I was intended to provide a random activity, so I made it so that you cannot alter the number of participants or change the price requirements. Maybe I will change that in the future.</li>
-                        <li>If the activity suggests more participants than you have, feel free to get creative. For example: "Go to an escape room" suggests that four participants be involved. If you are alone or have fewer than four participants, you can take this opportunity to go to your favorite social media site and recruit others. Don't count it out just because you might not have the exact number of participants.</li>
-                        <li>Lastly, the price indicator is there to give an indication of whether or not you might need money for the activity. This is a best guess and not an exact science.</li>
-                    </ol>
-                    <p>Do remember that each time you press the button, all of the previous information goes away never to return again. Just kidding, but it will go away until it is randomly generated again. Have fun!!</p>
                     <button type="button" class="close">Let's Get Started!</button>
                 </div>
             </div>     
