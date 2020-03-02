a88fa536100d6b35a0baecadea92f25402534add
David B <d.jbradley.ii@gmail.com>
2020-03-02 13:49:58 -0800

    Adds logic for a load-msg indicator & separates it from the error message indicator

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function getYouTubeResults(activity){
     .then(responseJson => {
         displayYouTubeResults(responseJson);
         $('.error-msg').text(` `);
+        $('.load-msg').text(` `);
     })
     .catch( error => {
         $('.error-msg').text(`Something went wrong: ${error.message}`);
@@ -87,7 +88,8 @@ function getActivity(activityType,accessIndex){
     })
     .then(responseJson => {
         displayActivity(responseJson);
-        $('.error-msg').text(`Loading YouTube Videos...`);
+        $('.error-msg').text(` `);
+        $('.load-msg').text(`Loading YouTube Videos...`);
     })
     .catch(error => {
         $('.error-msg').text(`Something went wrong: ${error.message}`);
@@ -96,7 +98,8 @@ function getActivity(activityType,accessIndex){
 
 function handleFormSubmission(){
     $('#activity-form').on('submit', function(evt){
-        $('.error-msg').text(`Loading Activity & YouTube Videos...`);
+        $('.error-msg').text(` `);
+        $('.load-msg').text(`Loading Activity & YouTube Videos...`);
         let activityType = $('#activity-type').val();
         let accessIndex = $('#access-index').prop('checked');
         $('.youtube-results-list').empty();
